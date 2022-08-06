b68bc931313b761ec88ef61b83dbd530dab4d5ea
Tiago Silva <tfn.da.silva@gmail.com>
2022-08-06 20:19:34 +0100

    Remove title
    To save space, title was moved to the Tableau dashboard and removed from App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MainDashboard from './components/MainDashboard';
 function App() {
   return (
     <div className="App">
-      <h1>Bancor Analytics Dashboard</h1>
       <MainDashboard />
     </div>
   );
